4eed57b1136651c915af93b6b607257e53300879
Jyun <njyun666666@gmail.com>
2023-07-14 13:38:24 +0800

    fix

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  style: {
+    postcss: {
+      loaderOptions: (postcssLoaderOptions) => {
+        postcssLoaderOptions.postcssOptions.plugins = [
+          require('tailwindcss/nesting'),
+          require('tailwindcss'),
+          'postcss-flexbugs-fixes',
+          [
+            'postcss-preset-env',
+            {
+              autoprefixer: {
+                flexbox: 'no-2009',
+              },
+              stage: 0,
+            },
+          ],
+        ];
+
+        return postcssLoaderOptions;
+      },
+    },
+  },
+};
