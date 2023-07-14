2b910bb6847606e144c1fb520b61759cd7949c57
Jyun <njyun666666@gmail.com>
2023-07-14 15:21:55 +0800

    set basename

--- src/router.tsx
+++ src/router.tsx
@@ -3,25 +3,28 @@ import DashboardLayout from './layouts/DashboardLayout';
 import DashboardPage from './pages/dashboard/DashboardPage';
 import ButtonPage from './pages/button/ButtonPage';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <DashboardLayout />,
-    children: [
-      {
-        path: '/',
-        element: <DashboardPage />,
-      },
-      {
-        path: 'button',
-        element: <ButtonPage />,
-      },
-    ],
-  },
-  {
-    path: '*',
-    element: <Navigate to="/" replace />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <DashboardLayout />,
+      children: [
+        {
+          path: '/',
+          element: <DashboardPage />,
+        },
+        {
+          path: 'button',
+          element: <ButtonPage />,
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <Navigate to="/" replace />,
+    },
+  ],
+  { basename: process.env.REACT_APP_BASENAME }
+);
 
 export default router;
