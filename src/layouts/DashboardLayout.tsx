86121f52aa78507b67dc6ea7c3be499086243590
Jyun <njyun666666@gmail.com>
2023-07-25 14:12:42 +0800

    scrollbar style

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -8,8 +8,8 @@ const DashboardLayout = () => {
       <div className="fixed top-0 z-[100] w-full border-b bg-[--color-header-bg]">
         <Header />
       </div>
-      <div className="h-screen">
-        <div className="fixed top-12 h-[calc(100%-theme(height.12))] w-60 overflow-y-auto bg-[--color-header-bg]">
+      <div className="h-screen scrollbar">
+        <div className="fixed top-12 h-[calc(100%-theme(height.12))] w-60 hover:overflow-y-auto bg-[--color-header-bg]">
           <Nav />
         </div>
         <div className="pl-60 pt-12">
