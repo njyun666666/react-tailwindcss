aed2ade5e47f25812023a6c656366c65481e3aa8
Jyun <njyun666666@gmail.com>
2023-08-11 16:10:04 +0800

    add Tree component

---
+++ src/components/tree/Tree.tsx
@@ -0,0 +1,140 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import React, { forwardRef, useRef, useState } from 'react';
+
+export interface TreeProps {
+  data: TreeDataProps[];
+  className?: string;
+}
+
+export interface TreeDataProps {
+  id: string;
+  content: React.ReactNode;
+  children?: TreeDataProps[];
+  expanded?: boolean;
+  childrenRef?: React.RefObject<HTMLUListElement>;
+}
+
+interface TreeExpandProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  showIcon?: boolean;
+  expanded?: boolean;
+}
+
+const TreeExpandIcon = ({ showIcon, expanded, className, ...rest }: TreeExpandProps) => {
+  return (
+    <div {...rest} className={classNames('Tree-expand-icon w-6 h-6 flex justify-center items-center', className)}>
+      {showIcon && (
+        <FontAwesomeIcon
+          icon={'angle-right'}
+          className={classNames('transition-[transform] duration-300 ease-in-out', {
+            'rotate-0': !expanded,
+            'rotate-90': expanded,
+          })}
+        />
+      )}
+    </div>
+  );
+};
+
+export const Tree = forwardRef<HTMLUListElement, TreeProps>(function TreeComponent(
+  { data, className }: TreeProps,
+  ref,
+) {
+  const [list, setList] = useState(data);
+
+  const handleClick = (data: TreeDataProps) => {
+    let offsetHeight = data.childrenRef?.current?.offsetHeight;
+
+    if (data.childrenRef?.current) {
+      if (data.expanded) {
+        data.childrenRef.current.style.height = `${offsetHeight}px`;
+        data.childrenRef?.current?.offsetHeight;
+      } else {
+        data.childrenRef.current.style.height = 'auto';
+        offsetHeight = data.childrenRef?.current?.offsetHeight;
+
+        data.childrenRef.current.style.height = '0px';
+        data.childrenRef?.current?.offsetHeight;
+      }
+    }
+
+    const targetExpanded = !data.expanded;
+
+    const newList = list.map((item) => {
+      if (item.id === data.id) {
+        item.expanded = targetExpanded;
+      }
+      return item;
+    });
+
+    if (data.childrenRef?.current) {
+      if (targetExpanded) {
+        data.childrenRef.current.style.height = `${offsetHeight}px`;
+      } else {
+        data.childrenRef.current.style.height = `0px`;
+      }
+    }
+
+    setList(newList);
+
+    setTimeout(() => {
+      if (data.childrenRef?.current) {
+        if (targetExpanded) {
+          data.childrenRef.current.style.height = 'auto';
+        }
+      }
+    }, 300);
+  };
+
+  return (
+    <ul className={classNames('Tree w-full', className)} ref={ref}>
+      {list.map((item) => {
+        const ilStyle = classNames('flex items-center mb-1', {});
+        const expandIconStyle = classNames('rounded hover:transparent-alpha cursor-pointer');
+        const contentStyle = classNames('', {});
+
+        if (item.children) {
+          item.childrenRef = useRef<HTMLUListElement>(null);
+        }
+
+        return (
+          <React.Fragment key={item.id}>
+            {!item.children ? (
+              <li className={ilStyle}>
+                <TreeExpandIcon />
+                <div className={contentStyle}>{item.content}</div>
+              </li>
+            ) : (
+              // have children
+              <li>
+                <div className={classNames(ilStyle)}>
+                  <TreeExpandIcon
+                    showIcon
+                    expanded={item.expanded}
+                    className={expandIconStyle}
+                    onClick={() => handleClick(item)}
+                  />
+
+                  <div className={contentStyle}>{item.content}</div>
+                </div>
+
+                <Tree
+                  data={item.children}
+                  className={classNames(
+                    'Tree-children pl-4 overflow-hidden will-change-[height] transition-[height] duration-300 ease-in-out',
+                    {
+                      'h-0': !item.expanded,
+                    },
+                  )}
+                  ref={item.childrenRef}
+                />
+              </li>
+            )}
+          </React.Fragment>
+        );
+      })}
+    </ul>
+  );
+});
+
+export default Tree;
---
+++ src/pages/tree/TreePage.tsx
@@ -0,0 +1,82 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import Tree, { TreeDataProps } from '../../components/tree/Tree';
+import Page from '../Page';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+
+interface MenuTextProps {
+  icon: IconProp;
+  text: string;
+  className?: string;
+}
+
+const MenuText = ({ icon, text, className }: MenuTextProps) => {
+  return (
+    <div className={classNames(className)}>
+      <FontAwesomeIcon icon={icon} className="pr-1 w-5" />
+      <span>{text}</span>
+    </div>
+  );
+};
+
+const menu: TreeDataProps[] = [
+  {
+    id: '1',
+    content: <MenuText icon={'home'} text="Home" />,
+  },
+  {
+    id: '2',
+    content: <MenuText icon={'star'} text="Button" />,
+    children: [
+      {
+        id: '2-1',
+        content: <MenuText icon={'star'} text="Button 2-1" />,
+      },
+      {
+        id: '2-2',
+        content: <MenuText icon={'star'} text="Button 2-2" />,
+      },
+    ],
+  },
+  {
+    id: '3',
+    content: <MenuText icon={'tree'} text="Tree" />,
+    children: [
+      {
+        id: '3-1',
+        content: <MenuText icon={'tree'} text="Tree 3-1" />,
+      },
+      {
+        id: '3-2',
+        content: <MenuText icon={'tree'} text="Tree 3-2" />,
+      },
+      {
+        id: '3-3',
+        content: <MenuText icon={'gear'} text="Tree 3-3" />,
+        children: [
+          {
+            id: 'Tree x-1',
+            content: <MenuText icon={'gear'} text="Tree x-1" />,
+          },
+          {
+            id: 'Tree x-2',
+            content: <MenuText icon={'gear'} text="Tree x-2" />,
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const TreePage = () => {
+  return (
+    <Page title="Tree">
+      <h1>Tree</h1>
+
+      <Tree data={menu} className="" />
+
+      <div className="my-10"></div>
+    </Page>
+  );
+};
+export default TreePage;
